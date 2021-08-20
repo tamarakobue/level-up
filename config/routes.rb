d59f24130902442e5eaa931fcac89d91694be925
jarvistamara <tamara.jarvis17@icloud.com>
2021-08-19 20:38:22 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  post "/register", to: "users#create"
+  get "/me", to: "users#show"
+
+  get "/levels/all", to: "levels#indexAll"
+
+  resources :levels do
+    resources :scores
+  end
+
+  resources :users do
+    resources :scores
+  end
+
+  resources :scores
 end
