54c867b6906d1b038673e74b09cf42b7995b72ea
jarvistamara <tamara.jarvis17@icloud.com>
2021-08-19 20:57:58 -0400

    controllers

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -1,2 +1,46 @@
 class LevelsController < ApplicationController
+    before_action :authorized
+
+    def index
+        user = User.find_by(id: session[:user_id])
+        levels = user.levels.uniq
+        render json: levels
+    end
+
+    def indexAll
+        levels = level.all
+        render json: levels, include: :scores
+    end
+
+    def create
+        user = User.find_by(id: session[:user_id])
+        level = user.levels.create(level_params)
+        if level
+            render json: level, status: :created
+        else
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        user = User.find_by(id: session[:user_id])
+        level = user.levels.find_by(id: params[:id])
+        if level
+            levs = level.scores
+            levs.destroy_all 
+            head :no_content   
+        else
+            render json: { error: "Not Authorized"}, status: :unauthorized
+        end
+    end
+
+    private 
+
+    def level_params
+        params.permit(:level_difficulty)
+    end
+
+    def authorized
+        return render json: {error: "unauthorized" }, status: :unauthorized unless session.include? :user_id
+    end
 end
--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,2 +1,70 @@
 class ScoresController < ApplicationController
+    before_action :authorized
+
+    def index
+        user = User.find_by(id: session[:user_id])
+        level = user.levels.find(params[:level_id])
+        scores = level.scores.where(user_id: user.id)
+        render json: scores, include: :level
+    end
+
+    def create
+        user = User.find_by(id: session[:user_id])
+        if score_params[:level_attributes]
+            score = user.scores.create(score_params)
+            if score.valid?
+                render json: score, include: :level, status: :created
+            else
+                render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+            end
+        elsif score_params[:level_id]
+            score = user.scores.create(points: score_params[:points], user_id: score_params[:user_id], level_id: score_params[:level_id])
+            if score.valid?
+                render json: score, include: :level, status: :created
+            else
+                render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+            end
+        else
+            level = user.levels.find_by(id: params[:level_id])
+            new_score = level.scores.create(score_params)
+            if new_score[:points] == nil ||  new_score[:user_id] == nil || new_score[:level_id] == nil
+                render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+            else
+                user.scores << new_score
+                render json: new_score, include: :level, status: :created
+            end 
+        end
+    end
+
+    def update
+        user = User.find_by(id: session[:user_id])
+        score = user.scores.find_by(id: params[:id])
+        if score
+            score.update(score_params)
+            render json: score, include: :level
+        else
+            render json: { error: "Not Authorized"}, status: :unauthorized
+        end
+    end
+
+    def destroy
+        user = User.find_by(id: session[:user_id])
+        score = user.scores.find_by(id: params[:id])
+        if score
+            score.destroy
+            head :no_content   
+        else
+            render json: { error: "Not Authorized"}, status: :unauthorized
+        end
+    end
+
+    private 
+
+    def score_params
+        params.require(:score).permit(:points, :user_id, :level_id, level_attributes: [:level_difficulty])
+    end
+
+    def authorized
+        return render json: {error: "unauthorized" }, status: :unauthorized unless session.include? :user_id
+    end
 end
