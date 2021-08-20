112f14b8c512611cb70b6d16d553412f4b61a7b6
jarvistamara <tamara.jarvis17@icloud.com>
2021-08-19 21:28:17 -0400

    Update ToggleLevels.js

--- client/src/components/ToggleLevels.js
+++ client/src/components/ToggleLevels.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const ToggleLevels = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+class ToggleLevels extends Component {
+
+    state = {
+        on: false
+    }
+
+    toggle = () => {
+        this.setState({
+            on: !this.state.on
+        })
+    }
+
+    render() {
+        const { children } = this.props
+        return  children({
+            on: this.state.on,
+            toggle: this.toggle
+        })
+    }
 }
-
 export default ToggleLevels
