dfc11f123f4ea6bc4ac89e2f8ba5a2f41e9e8bc2
jarvistamara <tamara.jarvis17@icloud.com>
2021-08-19 21:28:47 -0400

    Update GameOver.js

--- client/src/components/GameOver.js
+++ client/src/components/GameOver.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 
-const GameOver = () => {
+const GameOver = ({ finalScore, toggle }) => {
+  useEffect(() => {
+    fetch("http://localhost:3001/users")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("updated user score", data);
+      });
+  }, []);
+
+  if (finalScore < 45) {
     return (
-        <div>
-            
-        </div>
-    )
-}
-
-export default GameOver
+      <div className="game-container">
+        <h1>Oh, No. Time is up. Better luck next time.</h1>
+        <p>Your Final Score is: {finalScore}! You must get a total of 45 points to move to next level.</p>
+        <button className='button-warning pure-button' onClick={toggle}>Try Again</button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="game-container">
+        <h1>Yay! You won!</h1>
+        <p>Your Final Score is: {finalScore}! Way to Go!.</p>
+        <button className='button-warning pure-button' onClick={toggle}>Next Level</button>
+      </div>
+    );
+  }
+};
+export default GameOver;
